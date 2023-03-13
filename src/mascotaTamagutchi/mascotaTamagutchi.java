5874bccda32b2a51d1e5302495520685d8bcafc1
jcarlos9h <al9omeda@gmail.com>
2023-03-12 23:55:32 -0300

    rama de prueba de la mascota

---
+++ src/mascotaTamagutchi/mascotaTamagutchi.java
@@ -0,0 +1,12 @@
+package mascotaTamagutchi;
+
+import javax.sound.midi.Soundbank;
+
+public class mascotaTamagutchi {
+
+	public static void main(String[] args) {
+		String nombre="firulais";
+		System.out.println(nombre);
+	}
+	
+}
