f2d2fb74ae4765ef12399ceb82e7bfa66190788b
jcarlos9h <al9omeda@gmail.com>
2023-03-13 00:01:36 -0300

    probando ramas

--- src/mascotaTamagutchi/mascotaTamagutchi.java
+++ src/mascotaTamagutchi/mascotaTamagutchi.java
@@ -1,12 +1,11 @@
 package mascotaTamagutchi;
 
-import javax.sound.midi.Soundbank;
-
 public class mascotaTamagutchi {
 
 	public static void main(String[] args) {
 		String nombre="firulais";
 		System.out.println(nombre);
+		System.out.println("hello word");
 	}
 	
 }
